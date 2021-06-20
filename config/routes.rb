b6fa63141d1515975a35c875ee98c71860a91c75
Jason McKenzie <jmac.atx@gmail.com>
2021-06-20 14:44:54 -0500

    adds query, segment and body routes to display actor by id

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,10 @@ Rails.application.routes.draw do
   get "/all_movies" => "movies#all_movies"
 
   get "/one_movie/:id" => "movies#one_movie_by_id"
+
+  get "/query_actor" => "actors#display_actor"
+
+  get "segment_actor/:id" => "actors#display_actor"   ## alternate to line 4 above
+
+  post "body_actor" => "actors#display_actor"
 end
