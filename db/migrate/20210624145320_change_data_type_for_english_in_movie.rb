cee1b3ba9e397f1be808e337b95b8dc9f54ccc54
Jason McKenzie <jmac.atx@gmail.com>
2021-06-24 10:40:48 -0500

    updates movies controller with director and english attibutes

--- db/migrate/20210624145320_change_data_type_for_english_in_movie.rb
+++ db/migrate/20210624145320_change_data_type_for_english_in_movie.rb
@@ -1,5 +1,6 @@
 class ChangeDataTypeForEnglishInMovie < ActiveRecord::Migration[6.1]
   def change
-    change_column :movies, :english, :boolean, using: "english::boolean", default: true
+    change_column :movies, :english, :boolean, null: false, default: true
+    # change_column :movies, :english, :boolean, using: "english::boolean", null: false, default: true
   end
 end
