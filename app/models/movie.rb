fe3a38d0f2887f3e0f7475c7478b89e441c746ac
Jason McKenzie <jmac.atx@gmail.com>
2021-06-28 09:58:32 -0500

    adds sorting actions to actor and movie models

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ApplicationRecord
   validates :title, presence: true
   validates :year, numericality: { greater_than: 1800 }
-  validates :plot, length: {greater than 20}
+  # validates :plot, length: { greater_than: 20 }
   validates :director, presence: true
 end
